727e91d5295197bba7a5ac042a24c8782604dd78
jimmyberg <jimmyberg.kim@gmail.com>
2021-08-29 15:24:42 +0900

    ems project initialize

---
+++ src/test/kotlin/com/ems/core/DemoEmsCoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ems.core
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoEmsCoreApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
